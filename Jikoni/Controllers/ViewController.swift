65f594c3a3f383c353bebce87826c48dae54704b
WilsonMungai <wilsonmuguthi@gmail.com>
2023-01-23 22:27:32 -0500

    Getting started

---
+++ Jikoni/Controllers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Jikoni
+//
+//  Created by Wilson Mungai on 2023-01-23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
